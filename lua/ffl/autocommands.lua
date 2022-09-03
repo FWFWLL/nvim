f071289db5ea53925822772d42c976fd04c7d1af
FWFWLL <ffl@vivaldi.net>
2022-09-03 21:18:59 +0200

    Added highlighting on yank

--- lua/ffl/autocommands.lua
+++ lua/ffl/autocommands.lua
@@ -46,3 +46,10 @@ vim.api.nvim_create_autocmd({"BufWinEnter"}, {
 		vim.cmd "set formatoptions-=cro"
 	end,
 })
+
+-- Highlight Yanked Text
+vim.api.nvim_create_autocmd({"TextYankPost"}, {
+	callback = function()
+		vim.highlight.on_yank {higroup = "Visual", timeout = 200}
+	end,
+})
