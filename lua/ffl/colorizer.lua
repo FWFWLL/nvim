6fa749721d90b012d775ed32931d3a302f4a573a
FWFWLL <ffl@vivaldi.net>
2022-09-13 08:57:04 +0200

    Updated nvim-colorizer

--- lua/ffl/colorizer.lua
+++ lua/ffl/colorizer.lua
@@ -3,4 +3,14 @@ if not status_ok then
 	return
 end
 
-colorizer.setup {}
+colorizer.setup ({"*"}, {
+	RGB = true,
+	RRGGBB = true,
+	names = false,
+	RRGGBBAA = true,
+	rgb_fn = true,
+	hsl_fn = true,
+	css = false,
+	css_fn = false,
+	mode = "background",
+})
