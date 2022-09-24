9f71b18c1bf2e1bffd5a5dd646dbdf8252dba0e0
FWFWLL <ffl@vivaldi.net>
2022-09-24 16:39:41 +0200

    Update LSP settings

--- lua/ffl/lsp/settings/arduino_language_server.lua
+++ lua/ffl/lsp/settings/arduino_language_server.lua
@@ -2,8 +2,8 @@ return {
 	cmd = {
 		"arduino-language-server",
 		"-cli-config", "/home/ffl/.arduino15/arduino-cli.yaml",
-		"-fqbn", "arduino:avr",
-		"-cli", "arduino-cli",
-		"-clangd", "clangd",
+		"-fqbn", "arduino:avr:uno",
+		"-cli", "/home/ffl/Downloads/bin/arduino-cli",
+		"-clangd", "home/ffl/.local/share/nvim/mason/bin/clangd",
 	},
 }
