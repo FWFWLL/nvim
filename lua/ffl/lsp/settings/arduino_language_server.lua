05283b1f7c720c4460e2dcfc56bfdfaa36c948ce
FWFWLL <ffl@vivaldi.net>
2022-09-25 11:37:00 +0200

    Fix configuration of arduino_language_server

--- lua/ffl/lsp/settings/arduino_language_server.lua
+++ lua/ffl/lsp/settings/arduino_language_server.lua
@@ -3,7 +3,7 @@ return {
 		"arduino-language-server",
 		"-cli-config", "/home/ffl/.arduino15/arduino-cli.yaml",
 		"-fqbn", "arduino:avr:uno",
-		"-cli", "/home/ffl/Downloads/bin/arduino-cli",
-		"-clangd", "home/ffl/.local/share/nvim/mason/bin/clangd",
+		"-cli", "/home/ffl/.config/nvim/arduino-cli/arduino-cli",
+		"-clangd", "/home/ffl/.local/share/nvim/mason/bin/clangd",
 	},
 }
