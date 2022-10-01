b7174c59c5ae5bebc2f8b1cf9ff5ef49c74a4c14
FWFWLL <ffl@vivaldi.net>
2022-10-01 21:33:48 +0200

    Add `null-ls` sources

--- lua/ffl/lsp/null-ls.lua
+++ lua/ffl/lsp/null-ls.lua
@@ -3,6 +3,13 @@ if not status_ok then
 	return
 end
 
+local code_actions = null_ls.builtins.code_actions
+local formatting = null_ls.builtins.formatting
+
 null_ls.setup {
 	debug = false,
+	sources = {
+		code_actions.eslint,
+		formatting.astyle,
+	}
 }
