f9e2e18a4c798e45f9cc68245bb34d741d6d2887
FWFWLL <ffl@vivaldi.net>
2022-10-30 13:20:39 +0100

    Update `toggleterm.nvim`

--- lua/ffl/toggleterm.lua
+++ lua/ffl/toggleterm.lua
@@ -3,6 +3,8 @@ if not status_ok then
 	return
 end
 
+local opts = {noremap = true, silent = true}
+
 toggleterm.setup {
 	size = function(term)
 		if term.direction == "horizontal" then
@@ -12,6 +14,12 @@ toggleterm.setup {
 		end
 	end,
 	open_mapping = [[<C-S-\>]],
+	on_open = function()
+		vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-w>h]], opts)
+		vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-w>j]], opts)
+		vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-w>k]], opts)
+		vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-w>l]], opts)
+	end,
 	hide_numbers = true,
 	shade_filetypes = {},
 	shade_terminals = true,
@@ -31,20 +39,6 @@ toggleterm.setup {
 		},
 	},
 }
-
-local opts = {noremap = true, silent = true}
-
--- Terminal Mappings
-function _G.set_terminal_keymaps()
-	opts = {noremap = true}
-
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-w>h]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-w>j]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-w>k]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-w>l]], opts)
-end
-
-vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
 
 local Terminal = require("toggleterm.terminal").Terminal
 
@@ -92,47 +86,3 @@ end
 
 vim.keymap.set("n", [[<C-\>]], "<CMD>lua _FLOAT_TERM()<CR>", opts)
 vim.keymap.set("i", [[<C-\>]], "<CMD>lua _FLOAT_TERM()<CR>", opts)
-
--- Vertical Left-Docked Terminal
-local vertical_term = Terminal:new {
-	direction = "vertical",
-	on_open = function(term)
-		local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(term.bufnr, ...) end
-
-		vim.cmd "startinsert!"
-
-		buf_set_keymap("n", "<M-0>", "<CMD>2ToggleTerm size=60 direction=vertical<CR>", opts)
-		buf_set_keymap("t", "<M-0>", "<CMD>2ToggleTerm size=60 direction=vertical<CR>", opts)
-		buf_set_keymap("i", "<M-0>", "<CMD>2ToggleTerm size=60 direction=vertical<CR>", opts)
-	end,
-	count = 2,
-}
-
-function _VERTICAL_TERM()
-	vertical_term:toggle()
-end
-
-vim.keymap.set("n", "<M-0>", "<CMD>lua _VERTICAL_TERM()<CR>", opts)
-vim.keymap.set("i", "<M-0>", "<CMD>lua _VERTICAL_TERM()<CR>", opts)
-
--- Horizontal Bottom-Docked Terminal
-local horizontal_term = Terminal:new {
-	direction = "horizontal",
-	on_open = function(term)
-		local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(term.bufnr, ...) end
-
-		vim.cmd "startinsert!"
-
-		buf_set_keymap("n", "<M-9>", "<CMD>3ToggleTerm size=20 direction=horizontal_term<CR>", opts)
-		buf_set_keymap("t", "<M-9>", "<CMD>3ToggleTerm size=20 direction=horizontal_term<CR>", opts)
-		buf_set_keymap("i", "<M-9>", "<CMD>3ToggleTerm size=20 direction=horizontal_term<CR>", opts)
-	end,
-	count = 3,
-}
-
-function _HORIZONTAL_TERM()
-	horizontal_term:toggle()
-end
-
-vim.keymap.set("n", "<M-9>", "<CMD>lua _HORIZONTAL_TERM()<CR>", opts)
-vim.keymap.set("i", "<M-9>", "<CMD>lua _HORIZONTAL_TERM()<CR>", opts)
