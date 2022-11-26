336531dd1fba806adc2e4d159e6bb80a21b88ffc
FWFWLL <ffl@vivaldi.net>
2022-11-26 18:30:40 +0100

    Add catppuccin integration to native_lsp

--- lua/ffl/catppuccin.lua
+++ lua/ffl/catppuccin.lua
@@ -35,6 +35,21 @@ catppuccin.setup {
 	color_overrides = {},
 	custom_highlights = {},
 	integrations = {
+		native_lsp = {
+			enabled = true,
+			virtual_text = {
+				errors = {"italic"},
+				hints = {"italic"},
+				warnings = {"italic"},
+				information = {"italic"},
+			},
+			underlines = {
+				errors = {"underline"},
+				hints = {"underline"},
+				warnings = {"underline"},
+				information = {"underline"},
+			},
+		},
 		nvimtree = true,
 	},
 }
