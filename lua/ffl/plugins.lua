0c62ab7bd6007516d21caa4003a6a771ad5def1d
FWFWLL <ffl@vivaldi.net>
2022-12-14 21:19:09 +0100

    Add dressing.nvim

--- lua/ffl/plugins.lua
+++ lua/ffl/plugins.lua
@@ -57,6 +57,7 @@ return packer.startup(function(use)
 
 	-- Utility
 	use("saifulapm/chartoggle.nvim") -- Toggles any character at the end of line
+	use("stevearc/dressing.nvim") -- Improve the built-in vim.ui interfaces
 
 	-- Terminal Integration
 	use({"akinsho/toggleterm.nvim", tag = "*"}) -- Neovim plugin to help easily manage multiple terminal windows
