546975ad6727d023cf0cd568a3a99b0019cf25e7
FWFWLL <ffl@vivaldi.net>
2023-02-01 17:33:58 +0100

    Change to the new way of opening nvim-tree at startup

--- lua/ffl/plugins/nvim-tree.lua
+++ lua/ffl/plugins/nvim-tree.lua
@@ -1,6 +1,6 @@
 local M = {"nvim-tree/nvim-tree.lua"}
 
-M.event = "VeryLazy"
+M.lazy = false
 M.dependencies = {
 	"nvim-tree/nvim-web-devicons",
 }
@@ -137,6 +137,30 @@ M.config = function()
 				quit_on_open = true,
 			},
 		},
+	})
+
+	local function open_nvim_tree(data)
+		-- Buffer is a [No Name]
+		local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+		-- Buffer is a directory
+		local directory = vim.fn.isdirectory(data.file) == 1
+
+		if not no_name and not directory then
+			return
+		end
+
+		-- Change to the directory
+		if directory then
+			vim.cmd.cd(data.file)
+		end
+
+		-- Open the tree
+		require("nvim-tree.api").tree.open()
+	end
+
+	vim.api.nvim_create_autocmd({"VimEnter"}, {
+		callback = open_nvim_tree,
 	})
 end
 
