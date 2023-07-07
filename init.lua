60ac9743e07a9e48c5c5eb81f36ede3a8d710892
FWFWLL <ffl@vivaldi.net>
2023-07-07 15:33:22 +0200

    Update Neovide settings

--- init.lua
+++ init.lua
@@ -3,24 +3,17 @@ vim.g.loaded_netrwPlugin = 1
 
 if vim.g.neovide then
 	vim.o.guifont = "ComicCodeLigatures Nerd Font:h12"
-
 	vim.g.neovide_padding_top = 8
 	vim.g.neovide_padding_bottom = 8
 	vim.g.neovide_padding_right = 8
 	vim.g.neovide_padding_left = 8
-
 	vim.g.neovide_transparency = 0.8
-
-	vim.g.neovide_scroll_animation_length = 0.5
-
+	vim.g.transparency = 0.8
 	vim.g.neovide_refresh_rate = 60
 	vim.g.neovide_refresh_rate_idle = 5
-
 	vim.g.neovide_fullscreen = true
-
-	vim.g.neovide_cursor_animation_length = 0.1
-
-	vim.g.neovide_cursor_vfx_mode = "railgun"
+	vim.g.neovide_cursor_animation_length = 0
+	vim.g.neovide_hide_mouse_when_typing = true
 end
 
 require("ffl.options")
