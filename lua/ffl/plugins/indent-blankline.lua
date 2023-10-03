d3f33e6b48c29dab1685157ea49c6de2220309ac
FWFWLL <ffl@vivaldi.net>
2023-10-03 10:47:19 +0200

    Update `indent-blankline.nvim` to version 3

--- lua/ffl/plugins/indent-blankline.lua
+++ lua/ffl/plugins/indent-blankline.lua
@@ -5,25 +5,27 @@ M.event = "BufReadPost"
 M.config = function()
 	local preq = require("ffl.functions").preq
 
-	local status_ok, indent_blankline = preq("indent_blankline")
+	local status_ok, indent_blankline = preq("ibl")
 	if not status_ok then
 		return
 	end
 
 	indent_blankline.setup({
-		use_treesitter = true,
-		show_first_indent_level = false,
-		show_trailing_blankline_indent = false,
-		show_current_context = true,
-		filetype_exclude = {
-			"help",
-			"checkhealth",
-			"NvimTree",
+		scope = {
+			show_start = false,
+			show_end = false,
 		},
-		buftype_exclude = {
-			"terminal",
-			"prompt",
-			"nofile",
+		exclude = {
+			filetypes = {
+				"help",
+				"checkhealth",
+				"NvimTree",
+			},
+			buftypes = {
+				"terminal",
+				"prompt",
+				"nofile",
+			},
 		},
 	})
 end
